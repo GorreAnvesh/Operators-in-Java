c8ed6aeba6715b8e4ed6069d3096c939980cd0a4
GorreAnvesh <anveshandry@gmail.com>
2025-05-23 21:16:13 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class Arithmetic {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	System.out.println("ARITHMETIC OPERATORS");
+	System.out.println("sum : " + (a+b));
+	System.out.println("sub : " + (a-b));
+	System.out.println("mul : " + (a*b));
+	System.out.println("div : " + (a/b));
+	System.out.println("mod : " + (a%b));
+	System.out.println("");
+	System.out.println("RELATIONAL OPERATORS");
+	System.out.println("Less Than  <               : " + (a<b));
+	System.out.println("Greater Than  > 	   : " + (a>b));
+	System.out.println("EqualsTo  ==               : " + (a==b));
+	System.out.println("Not EqualsTo  !=           : " + (a!=b));
+	System.out.println("LessThan or EqualsTo  <=   : " + (a<=b));
+	System.out.println("GreaterThan or EqualsTo >= : " + (a>=b));
+	
+}
+}
